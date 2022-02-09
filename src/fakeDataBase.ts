2defa64ae098ab731165070d52743aa0aae41d41
Blaszyk Anthony <blaszyk.anthony@gmail.com>
2022-02-09 14:20:51 +0100

    Add view files and functionnal routes with nunjuks and express

---
+++ src/fakeDataBase.ts
@@ -0,0 +1,25 @@
+const objectArray = [
+  {
+    id: 0,
+    title: "World of Warcraft",
+    desc: "",
+    releaseYear: 2004,
+    link: "WorldOfWarcraft",
+  },
+  {
+    id: 1,
+    title: "The legend of zelda : Ocarina of Time",
+    desc: "",
+    releaseYear: 1998,
+    link: "ZeldaOOT",
+  },
+  {
+    id: 2,
+    title: "Gris",
+    desc: "",
+    releaseYear: 2018,
+    link: "Gris",
+  },
+];
+
+export { objectArray };
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
 import express from "express";
 import nunjucks from "nunjucks";
 
+import { objectArray } from "./fakeDataBase";
+
 const app = express();
+
+app.use(express.static("public"));
 
 nunjucks.configure("views", {
   autoescape: true,
   express: app,
+});
+
+app.set("view engine", "njk");
+
+app.get("/", (request, response) => {
+  response.render("home", { objectArray });
+});
+
+app.get("/test/:gameName", (request, response) => {
+  const routeParameters = request.params;
+
+  const findRoute = objectArray.find((route) => route.link === routeParameters.gameName);
+
+  if (findRoute) {
+    response.render("details", { name: routeParameters.gameName, objectArray, findRoute });
+  } else {
+    response.status(404).render("not-found", { error: "Game not found" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server started on http://localhost:3000");
 });
